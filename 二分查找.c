cc1ba24cd861cb226c54a7647b2f7c0976cc9dca
Chenmingyao1 <56284129+Chenmingyao1@users.noreply.github.com>
2020-09-06 23:56:47 +0800

    Add files via upload

---
+++ 二分查找.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int binary_search(int key,int a[],int n)
+{
+    int l=0,r=n-1,mid;
+    while(l<=r)
+    {
+         int mid=l+(r-l)/2;
+         if(a[mid]==key)
+         {
+             printf("%d\n",mid);
+             break;
+         }
+         if(key>a[mid])
+         {
+             l=mid+1;
+         }
+             else
+             {
+                 r=mid-1;
+             }
+             
+         
+    }
+return 0;
+}
+int main()
+{
+    int i,a[100],n,key;
+    printf("请输入数组长度：\n");
+    scanf("%d",&n);
+    printf("请输入数组元素：\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("请输入你想查找的元素：\n");
+    scanf("%d",&key);
+    binary_search(key,a,n);
+    printf("\n");
+    return 0;
+}
